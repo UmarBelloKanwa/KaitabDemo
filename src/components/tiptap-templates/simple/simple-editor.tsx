273a6244f34c8b984fac4034c6be3f3cdc16b4ef
Umar Bello Kanwa <umarbellokanwa@gmail.com>
2025-12-09 18:12:51 +0000

    Correct editor

--- src/components/tiptap-templates/simple/simple-editor.tsx
+++ src/components/tiptap-templates/simple/simple-editor.tsx
@@ -1,171 +1,128 @@
-"use client";
-
-import { useEffect, useRef, useState } from "react";
-import { EditorContent, EditorContext, useEditor } from "@tiptap/react";
-
+"use client"
+import { useEffect, useRef, useState } from "react"
+import { EditorContent, EditorContext, useEditor } from "@tiptap/react"
 // --- Tiptap Core Extensions ---
-import { StarterKit } from "@tiptap/starter-kit";
-import { Placeholder } from "@tiptap/extensions";
-import { Image } from "@tiptap/extension-image";
-import { TaskItem, TaskList } from "@tiptap/extension-list";
-import { TextAlign } from "@tiptap/extension-text-align";
-import { Typography } from "@tiptap/extension-typography";
-import { Highlight } from "@tiptap/extension-highlight";
-import { Subscript } from "@tiptap/extension-subscript";
-import { Superscript } from "@tiptap/extension-superscript";
-import { Selection } from "@tiptap/extensions";
-
+import { StarterKit } from "@tiptap/starter-kit"
+import { Placeholder } from "@tiptap/extensions"
+import { Image } from "@tiptap/extension-image"
+import { TaskItem, TaskList } from "@tiptap/extension-list"
+import { TextAlign } from "@tiptap/extension-text-align"
+import { Typography } from "@tiptap/extension-typography"
+import { Highlight } from "@tiptap/extension-highlight"
+import { Subscript } from "@tiptap/extension-subscript"
+import { Superscript } from "@tiptap/extension-superscript"
+import { Selection } from "@tiptap/extensions"
 // --- UI Primitives ---
-import { Button } from "@/components/tiptap-ui-primitive/button";
-import { Spacer } from "@/components/tiptap-ui-primitive/spacer";
-import {
-  Toolbar,
-  ToolbarGroup,
-  ToolbarSeparator,
-} from "@/components/tiptap-ui-primitive/toolbar";
-
+import { Button } from "@/components/tiptap-ui-primitive/button"
+import { Spacer } from "@/components/tiptap-ui-primitive/spacer"
+import { Toolbar, ToolbarGroup, ToolbarSeparator } from "@/components/tiptap-ui-primitive/toolbar"
 // --- Tiptap Node ---
-import { ImageUploadNode } from "@/components/tiptap-node/image-upload-node/image-upload-node-extension";
-import { HorizontalRule } from "@/components/tiptap-node/horizontal-rule-node/horizontal-rule-node-extension";
-import "@/components/tiptap-node/blockquote-node/blockquote-node.scss";
-import "@/components/tiptap-node/code-block-node/code-block-node.scss";
-import "@/components/tiptap-node/horizontal-rule-node/horizontal-rule-node.scss";
-import "@/components/tiptap-node/list-node/list-node.scss";
-import "@/components/tiptap-node/image-node/image-node.scss";
-import "@/components/tiptap-node/heading-node/heading-node.scss";
-import "@/components/tiptap-node/paragraph-node/paragraph-node.scss";
-
+import { ImageUploadNode } from "@/components/tiptap-node/image-upload-node/image-upload-node-extension"
+import { HorizontalRule } from "@/components/tiptap-node/horizontal-rule-node/horizontal-rule-node-extension"
+import "@/components/tiptap-node/blockquote-node/blockquote-node.scss"
+import "@/components/tiptap-node/code-block-node/code-block-node.scss"
+import "@/components/tiptap-node/horizontal-rule-node/horizontal-rule-node.scss"
+import "@/components/tiptap-node/list-node/list-node.scss"
+import "@/components/tiptap-node/image-node/image-node.scss"
+import "@/components/tiptap-node/heading-node/heading-node.scss"
+import "@/components/tiptap-node/paragraph-node/paragraph-node.scss"
 // --- Tiptap UI ---
-import { HeadingDropdownMenu } from "@/components/tiptap-ui/heading-dropdown-menu";
-import { ImageUploadButton } from "@/components/tiptap-ui/image-upload-button";
-import { ListDropdownMenu } from "@/components/tiptap-ui/list-dropdown-menu";
-import { BlockquoteButton } from "@/components/tiptap-ui/blockquote-button";
-import { CodeBlockButton } from "@/components/tiptap-ui/code-block-button";
+import { HeadingDropdownMenu } from "@/components/tiptap-ui/heading-dropdown-menu"
+import { ImageUploadButton } from "@/components/tiptap-ui/image-upload-button"
+import { ListDropdownMenu } from "@/components/tiptap-ui/list-dropdown-menu"
+import { BlockquoteButton } from "@/components/tiptap-ui/blockquote-button"
+import { CodeBlockButton } from "@/components/tiptap-ui/code-block-button"
 import {
   ColorHighlightPopover,
   ColorHighlightPopoverContent,
   ColorHighlightPopoverButton,
-} from "@/components/tiptap-ui/color-highlight-popover";
-import {
-  LinkPopover,
-  LinkContent,
-  LinkButton,
-} from "@/components/tiptap-ui/link-popover";
-import { MarkButton } from "@/components/tiptap-ui/mark-button";
-import { TextAlignButton } from "@/components/tiptap-ui/text-align-button";
-import { UndoRedoButton } from "@/components/tiptap-ui/undo-redo-button";
-
+} from "@/components/tiptap-ui/color-highlight-popover"
+import { LinkPopover, LinkContent, LinkButton } from "@/components/tiptap-ui/link-popover"
+import { MarkButton } from "@/components/tiptap-ui/mark-button"
+import { TextAlignButton } from "@/components/tiptap-ui/text-align-button"
+import { UndoRedoButton } from "@/components/tiptap-ui/undo-redo-button"
 // --- Icons ---
-import { ArrowLeftIcon } from "@/components/tiptap-icons/arrow-left-icon";
-import { HighlighterIcon } from "@/components/tiptap-icons/highlighter-icon";
-import { LinkIcon } from "@/components/tiptap-icons/link-icon";
-
+import { ArrowLeftIcon } from "@/components/tiptap-icons/arrow-left-icon"
+import { HighlighterIcon } from "@/components/tiptap-icons/highlighter-icon"
+import { LinkIcon } from "@/components/tiptap-icons/link-icon"
 // --- Hooks ---
-import { useIsBreakpoint } from "@/hooks/use-is-breakpoint";
-import { useWindowSize } from "@/hooks/use-window-size";
-import { useCursorVisibility } from "@/hooks/use-cursor-visibility";
-import { useTheme } from "@mui/material/styles";
-
+import { useIsBreakpoint } from "@/hooks/use-is-breakpoint"
+import { useWindowSize } from "@/hooks/use-window-size"
+import { useCursorVisibility } from "@/hooks/use-cursor-visibility"
+import { useTheme } from "@mui/material/styles"
 // --- Components ---
-import { ThemeToggle } from "@/components/tiptap-templates/simple/theme-toggle";
-import BackButton from "@/components/ui/common/BackButton";
-
+import { ThemeToggle } from "@/components/tiptap-templates/simple/theme-toggle"
+import BackButton from "@/components/ui/common/BackButton"
 // --- Lib ---
-import { handleImageUpload, MAX_FILE_SIZE } from "@/lib/tiptap-utils";
-
+import { handleImageUpload, MAX_FILE_SIZE } from "@/lib/tiptap-utils"
 // --- Styles ---
-import "@/components/tiptap-templates/simple/simple-editor.scss";
-
-import content from "@/components/tiptap-templates/simple/data/content.json";
+import "@/components/tiptap-templates/simple/simple-editor.scss"
 // import StyledEditorBox from "@/components/ui/editor/StyledEditorBox";
-import Box from "@mui/material/Box";
-import MuiButton from "@mui/material/Button";
-
+import Box from "@mui/material/Box"
+import MuiButton from "@mui/material/Button"
 const MainToolbarContent = ({
   onHighlighterClick,
   onLinkClick,
   isMobile,
 }: {
-  onHighlighterClick: () => void;
-  onLinkClick: () => void;
-  isMobile: boolean;
+  onHighlighterClick: () => void
+  onLinkClick: () => void
+  isMobile: boolean
 }) => {
   return (
     <>
       <Spacer />
-
       <ToolbarGroup>
         <UndoRedoButton action="undo" />
         <UndoRedoButton action="redo" />
       </ToolbarGroup>
-
       <ToolbarSeparator />
-
       <ToolbarGroup>
         <HeadingDropdownMenu levels={[1, 2, 3, 4]} portal={isMobile} />
-        <ListDropdownMenu
-          types={["bulletList", "orderedList", "taskList"]}
-          portal={isMobile}
-        />
+        <ListDropdownMenu types={["bulletList", "orderedList", "taskList"]} portal={isMobile} />
         <BlockquoteButton />
         <CodeBlockButton />
       </ToolbarGroup>
-
       <ToolbarSeparator />
-
       <ToolbarGroup>
         <MarkButton type="bold" />
         <MarkButton type="italic" />
         <MarkButton type="strike" />
         <MarkButton type="code" />
         <MarkButton type="underline" />
-        {!isMobile ? (
-          <ColorHighlightPopover />
-        ) : (
-          <ColorHighlightPopoverButton onClick={onHighlighterClick} />
-        )}
+        {!isMobile ? <ColorHighlightPopover /> : <ColorHighlightPopoverButton onClick={onHighlighterClick} />}
         {!isMobile ? <LinkPopover /> : <LinkButton onClick={onLinkClick} />}
       </ToolbarGroup>
-
       <ToolbarSeparator />
-
       <ToolbarGroup>
         <MarkButton type="superscript" />
         <MarkButton type="subscript" />
       </ToolbarGroup>
-
       <ToolbarSeparator />
-
       <ToolbarGroup>
         <TextAlignButton align="left" />
         <TextAlignButton align="center" />
         <TextAlignButton align="right" />
         <TextAlignButton align="justify" />
       </ToolbarGroup>
-
       <ToolbarSeparator />
-
       <ToolbarGroup>
         <ImageUploadButton text="Add" />
       </ToolbarGroup>
-
       <Spacer />
-
       {isMobile && <ToolbarSeparator />}
-
       <ToolbarGroup>
         <ThemeToggle />
       </ToolbarGroup>
     </>
-  );
-};
-
+  )
+}
 const MobileToolbarContent = ({
   type,
   onBack,
 }: {
-  type: "highlighter" | "link";
-  onBack: () => void;
+  type: "highlighter" | "link"
+  onBack: () => void
 }) => (
   <>
     <ToolbarGroup>
@@ -178,33 +135,24 @@ const MobileToolbarContent = ({
         )}
       </Button>
     </ToolbarGroup>
-
     <ToolbarSeparator />
-
-    {type === "highlighter" ? (
-      <ColorHighlightPopoverContent />
-    ) : (
-      <LinkContent />
-    )}
+    {type === "highlighter" ? <ColorHighlightPopoverContent /> : <LinkContent />}
   </>
-);
-
+)
 export function SimpleEditor() {
-  const theme = useTheme();
+  const theme = useTheme()
+  const [title, setTitle] = useState("")
+  const [subtitle, setSubtitle] = useState("")
+  const isMobile = useIsBreakpoint()
+  const { height } = useWindowSize()
+  const [mobileView, setMobileView] = useState<"main" | "highlighter" | "link">("main")
+  const toolbarRef = useRef<HTMLDivElement>(null)
+  const [isPreview, setIsPreview] = useState(false)
+  const [editorContent, setEditorContent] = useState<any>({} as any)
+  const [previewContent, setPreviewContent] = useState<any>({} as any)
 
-  const [title, setTitle] = useState("");
-  const [preview, setPreview] = useState(false);
-  const [subtitle, setSubtitle] = useState("");
-
-  const isMobile = useIsBreakpoint();
-  const { height } = useWindowSize();
-  const [mobileView, setMobileView] = useState<"main" | "highlighter" | "link">(
-    "main"
-  );
-  const toolbarRef = useRef<HTMLDivElement>(null);
-
-  const editor = useEditor({
-    immediatelyRender: false,
+  const inEditor = {
+    content: editorContent,
     editorProps: {
       attributes: {
         autocomplete: "off",
@@ -243,52 +191,80 @@ export function SimpleEditor() {
         onError: (error) => console.error("Upload failed:", error),
       }),
     ],
-    content: "",
-  });
+    immediatelyRender: false,
+    shouldRerenderOnTransaction: false,
+  }
+
+  const inPreview = {
+    editable: false, // make it read-only
+    content: previewContent, // initial content from main editor
+    extensions: [
+      StarterKit,
+      Image,
+      Highlight,
+      TextAlign.configure({ types: ["heading", "paragraph"] }),
+      TaskList,
+      TaskItem,
+      Superscript,
+      Subscript,
+      Typography,
+      HorizontalRule,
+    ],
+    immediatelyRender: false,
+    shouldRerenderOnTransaction: false,
+  }
+
+  const editor = useEditor({
+    ...inEditor,
+  })
+
+  const previewEditor = useEditor({
+    ...inPreview,
+  })
 
   const rect = useCursorVisibility({
     editor,
     overlayHeight: toolbarRef.current?.getBoundingClientRect().height ?? 0,
-  });
+  })
 
   useEffect(() => {
     if (!isMobile && mobileView !== "main") {
-      setMobileView("main");
+      setMobileView("main")
     }
-  }, [isMobile, mobileView]);
+  }, [isMobile, mobileView])
+
+  useEffect(() => {
+    if (isPreview && editor && previewEditor) {
+      const content = editor.getJSON()
+      previewEditor.commands.setContent(content)
+    }
+  }, [isPreview, editor, previewEditor])
 
   const togglePreview = () => {
-    setPreview((prev) => !prev);
-  };
+    if (!isPreview && editor && previewEditor) {
+      // Sync content when entering preview mode
+      const content = editor.getJSON()
+      previewEditor.commands.setContent(content)
+    }
+    setIsPreview(!isPreview)
+  }
 
   const handleSaveArticle = () => {
-    if (!editor) return;
-
+    if (!editor) return
     const articleData = {
       title,
       subtitle,
       contentHTML: editor.getHTML(),
       contentJSON: editor.getJSON(),
       createdAt: new Date().toISOString(),
-    };
-
-    console.log(" SAVED ARTICLE:", articleData);
-
+    }
+    console.log(" SAVED ARTICLE:", articleData)
     // Example:
     // await fetch('/api/article', { method: 'POST', body: JSON.stringify(articleData) })
-  };
-
-  useEffect(() => {
-    if (!editor) return;
-    editor.setEditable(!preview);
-  }, [preview, editor]);
+  }
 
   return (
-    <Box
-      component="div"
-      className="simple-editor-wrapper"
-      sx={{ width: "100%", maxWidth: "100%" }}
-    >
+    <Box component="div" className="simple-editor-wrapper" sx={{ width: "100%", maxWidth: "100%" }}>
       <EditorContext.Provider value={{ editor }}>
         <Box
           sx={{
@@ -299,16 +275,15 @@ export function SimpleEditor() {
           }}
         >
           <BackButton />
-
           <Box sx={{ marginLeft: "auto", display: "flex", gap: 1 }}>
             <MuiButton
               size="small"
-              variant={preview ? "contained" : "outlined"}
+              variant={isPreview ? "contained" : "outlined"}
               className={"elevated"}
               onClick={togglePreview}
               sx={{ textTransform: "none", px: 2 }}
             >
-              {preview ? "Edit" : "Preview"}
+              {isPreview ? "Edit" : "Preview"}
             </MuiButton>
             <MuiButton
               fullWidth
@@ -318,20 +293,15 @@ export function SimpleEditor() {
                 borderRadius: 2,
               }}
               onClick={() => {
-                handleSaveArticle();
+                handleSaveArticle()
               }}
             >
               Publish
             </MuiButton>
           </Box>
         </Box>
-        <Box
-          sx={{
-            maxWidth: 800,
-            mx: "auto",
-          }}
-        >
-          {!preview && (
+        <Box sx={{ maxWidth: 800, mx: "auto" }}>
+          {!isPreview && (
             <Toolbar
               ref={toolbarRef}
               style={{
@@ -362,7 +332,7 @@ export function SimpleEditor() {
               placeholder="Title"
               value={title}
               onChange={(e) => setTitle(e.target.value)}
-              readOnly={preview}
+              readOnly={isPreview}
               style={{
                 width: "100%",
                 fontSize: theme.typography.h3.fontSize,
@@ -380,7 +350,7 @@ export function SimpleEditor() {
               placeholder="Add a subtitle..."
               value={subtitle}
               onChange={(e) => setSubtitle(e.target.value)}
-              readOnly={preview}
+              readOnly={isPreview}
               style={{
                 width: "100%",
                 fontSize: theme.typography.body1.fontSize,
@@ -392,23 +362,15 @@ export function SimpleEditor() {
                 background: "transparent",
               }}
             />
-
-            {preview ? (
-              <EditorContent
-                editor={editor}
-                role="presentation"
-                className="simple-editor-content"
-              />
-            ) : (
-              <EditorContent
-                editor={editor}
-                role="presentation"
-                className="simple-editor-content"
-              />
-            )}
+            <div style={{ display: isPreview ? "none" : "block" }}>
+              <EditorContent editor={editor} role="presentation" className="simple-editor-content" />
+            </div>
+            <div style={{ display: isPreview ? "block" : "none" }}>
+              <EditorContent editor={previewEditor} role="presentation" className="simple-editor-content" />
+            </div>
           </Box>
         </Box>
       </EditorContext.Provider>
     </Box>
-  );
+  )
 }
