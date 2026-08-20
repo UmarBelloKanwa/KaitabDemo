ec4baeaebe3e87e21991fcb03cde6e77aec6b404
Umar Bello Kanwa <umarbellokanwa@gmail.com>
2026-08-20 11:22:35 +0000

    feat: update AuthCard back navigation to use router.back()

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,12 +2,14 @@
 
 import AuthCard from "@/components/ui/auth/AuthCard";
 import Container from "@mui/material/Container";
+import { useRouter } from "next/navigation";
 
 export default function LoginPage() {
-  
+  const router = useRouter();
+
   return (
     <Container maxWidth="sm">
-      <AuthCard displayAuthCard={true} setDisplayAuthCard={() => {}} />
+      <AuthCard displayAuthCard={true} setDisplayAuthCard={() => router.back()} />
     </Container>
   );
 }
